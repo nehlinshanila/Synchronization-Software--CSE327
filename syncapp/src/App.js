050b3194119a274693197b2017a2485555ea4044
nehlinshanila <nehlinshanila@gmail.com>
2023-05-23 16:09:48 +0600

    started frontend of syncapp

---
+++ syncapp/src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar/navbar";
+import Frontpage from "./components/Frontpage/frontpage";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Frontpage/>
+    </div>
+  )
+}
+
+export default App;
