f5bc8921b635b1eef227dc069a891c0670ce20b2
nehlinshanila <nehlinshanila@gmail.com>
2023-05-31 16:48:05 +0600

    flexbox

--- syncapp/src/components/Dashboard/Statistics/statisticsbar.jsx
+++ syncapp/src/components/Dashboard/Statistics/statisticsbar.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Bar } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
-import "../dashboard.css"; // Import the CSS file
+// import "../dashboard.css"; // Import the CSS file
 
 const StatisticsBar = ({ chartData }) => {
   return (
